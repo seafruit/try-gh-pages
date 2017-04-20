f2dd4d01f2d11b334ab59bce42bca52ddb87145c
seafruit <18829290140@163.com>
2017-04-20 16:38:40 +0800

    modified coaches-and-awards height

--- script/index.js
+++ script/index.js
@@ -94,3 +94,11 @@ function bling() {
   setTimeout('bling()', 300);
 }
 bling();
+//
+// function showLoading(){
+//   document.getElementById("over").style.display = "none";
+//   document.getElementById('layout').style.display = 'none';
+//   document.getElementById('main').style.display = 'block';
+// }
+//
+// setTimeout('showLoading()',5000);
